d12836ecea01af234c26e8a9cfd6e30cc31a3ee3
Pavel <vinogradovpavel32@gmail.com>
2019-03-06 19:54:28 +0300

    site

--- mySite_ide/urls.py
+++ mySite_ide/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib import admin
+from django.urls import include, path
+
 
 urlpatterns = [
     path('rsavenkov/', include('rsavenkov.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    url(r'vinog/', include('qwe1.mySite_ide.urls')),
 ]
--- pvinogradov/urls.py
+++ pvinogradov/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from first.page import views
+from mySite_ide import views
 
 urlpatterns = [
     url(r'^page1/', views.fn),
