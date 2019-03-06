be86d07aec666003aa9398c084f3882642ee1084
Qrowe <mikleshirinskiy@gmail.com>
2019-03-06 20:11:10 +0300

    Mshirinskiy app

--- mySite_ide/settings.py
+++ mySite_ide/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
-    'default': {
+    'mshirinskiy_db': {
         'NAME': 'Database',
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'postgres',
