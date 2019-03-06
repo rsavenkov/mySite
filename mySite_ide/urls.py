d7723ee5316862d11e6db4a385a36973c8a5197d
aleviev <arthurleviev@gmail.com>
2019-03-06 20:11:35 +0300

    aleviev

--- mySite_ide/urls.py
+++ mySite_ide/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('rsavenkov/', include('rsavenkov.urls')),
+    path('aleviev/',include('aleviev.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('mshirinskiy.urls')),
