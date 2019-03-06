f9016015861d33bf93f6b4c52108d3c4d962843e
fuerfenf <egor602@mail.ru>
2019-03-06 20:46:52 +0300

    start new

--- ezalitsky/models.py
+++ ezalitsky/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subone(models.Model):
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+
+class Subtwo(models.Model):
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
