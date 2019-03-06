fe6c4ea552d311d98764ed5302870d8245606b7d
kirilldrozdov <kirilldrozd12@gmail.com>
2019-03-06 22:00:37 +0300

    kd

--- mySite_ide/settings.py
+++ mySite_ide/settings.py
@@ -123,6 +123,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+    'Kddb': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+
 
 }
 
