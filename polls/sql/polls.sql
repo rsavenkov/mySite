4fa8f9ab40c10f82116d6faa4bcd47093f7ea8bd
rsavenkov <lubajnka@mail.ru>
2019-03-11 18:35:01 +0300

    Working poll application

---
+++ polls/sql/polls.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.polls_question (id, question_text, pub_date) VALUES (6, 'Из чего строится ООП код программы?', '2019-03-11 14:52:51.000000');
+INSERT INTO public.polls_question (id, question_text, pub_date) VALUES (7, 'Какие структуры данных существуют в Python?', '2019-03-11 14:57:23.000000');
+INSERT INTO public.polls_question (id, question_text, pub_date) VALUES (5, 'Язык программирования питон -', '2019-03-11 14:32:15.000000');
+
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (5, 'интерпретируемые и динамический', 0, 5);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (6, 'компилируемый и статический типизируемый', 0, 5);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (7, 'интерпретируемый и статически типизированный', 0, 5);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (8, 'компилируемый и динамический', 0, 5);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (9, 'из модулей и фукнций', 0, 6);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (10, 'из объектов и классов', 0, 6);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (11, 'из функций и лямбда-функций', 0, 6);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (12, 'списки и вектора', 0, 7);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (13, 'множества и индексы', 0, 7);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (14, 'кортежи и словари', 0, 7);
+INSERT INTO public.polls_choice (id, choice_text, votes, question_id) VALUES (15, 'кортежи, словари и списки', 0, 7);
