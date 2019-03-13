1e0fc9ef9555b723d5e78dcce6ce46eef847a0fb
Fullmacht <a0Pjkvrv>
2019-03-13 19:03:41 +0300

    удалил afrolov1

--- afrolov1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Afrolov1Config(AppConfig):
-    name = 'afrolov1'
--- afrolov1/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def abc(request):
-    return HttpResponse("My test page!")
