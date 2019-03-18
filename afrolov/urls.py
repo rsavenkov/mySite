485555b64cfb7d06c26040b05ecf390abebc041c
Fullmacht <a0Pjkvrv>
2019-03-18 22:03:10 +0300

    Готово

--- afrolov/urls.py
+++ afrolov/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
 
-from . import views
+from afrolov import views
 
 urlpatterns = [
-    path('example1', views.example1),
-    path('example2', views.example2),
-    # path('afrolov', views.afrolov, name='afrolov'),
-# ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.index),
 ]
+
--- afrolov/views.py
+++ afrolov/views.py
@@ -2,32 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-from afrolov.models import Question_afrolov
-from django.template import loader
+from afrolov.models import Question_afrolov, Choice_afrolov
+from django.shortcuts import render
 
-def example1(request):
-    return HttpResponse("Hello world!")
-
-def example2(request):
-    return HttpResponse("Hello world, again!")
 
 def index(request):
     latest_question_list = Question_afrolov.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('afrolov/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    latest_choice_list = Choice_afrolov.objects.all()
+    context = {'latest_question_list': latest_question_list,
+               'latest_choice_list': latest_choice_list}
+    return render(request, 'afrolov/index.html', context)
 
